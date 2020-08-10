87c1c363976f8d839873061063c55b9bc4eb3624
kangkid <530748277@qq.com>
2020-08-10 22:24:56 +0800

    testable

--- get_shop_info/src/get_shop_info.py
+++ get_shop_info/src/get_shop_info.py
@@ -7,15 +7,20 @@ import os
 print('Loading function')
 
 logger = logging.getLogger()
-def main_handler(event, context):
+def main_handler():
 
     logger.info("start main handler")
 
-    Host = os.getenv('host')
-    Port = os.getenv('port')
-    User = os.getenv('user')
-    Password = os.getenv('password')
-    DB = os.getenv('dbname')
+    Host="postgres-dwj1bc4n.sql.tencentcdb.com"
+    User="tencentdb_dwj1bc4n"
+    Password="8tpwd[0O32um|^P"
+    DB="tencentdb_dwj1bc4n"
+    Port="50140"
+    #Host = os.getenv('host')
+    #Port = os.getenv('port')
+    #User = os.getenv('user')
+    #Password = os.getenv('password')
+    #DB = os.getenv('dbname')
 	
     conn = psycopg2.connect(database=DB, user=User, password=Password, host=Host, port=Port)
     print "Opened database successfully"
